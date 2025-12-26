e2ee09b810d3ef61e07a2347c8408115698d982e
carlosr <carlosr@carlosrs-MacBook-Pro.local>
2025-12-25 21:43:10 -0800

    Fix: Resolve InvalidOperationException on Access Denied by adding custom AccessDenied.cshtml page

---
+++ Project65.Web/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Project65.Web.Pages.Account
+{
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
